30cbbbe1846371e6cda8e4bc7c97bd934b2858f9
Donald Biggers <DuG@tests-MacBook-Air.local>
2022-01-27 14:01:17 -0600

    all work orderTypeRoutes tested in insomnia(and work)

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes.js");
+const propertyRoutes = require("./property-routes");
+const requestRoutes = require("./request-routes");
+const tenantRoutes = require("./tenant-routes");
+const workOrderRoutes = require("./work-order-routes");
+
+router.use("/properties", propertyRoutes);
+router.use("/requests", requestRoutes);
+router.use("/tenants", tenantRoutes);
 router.use("/users", userRoutes);
+router.use("/workOrders", workOrderRoutes);
+
 module.exports = router;

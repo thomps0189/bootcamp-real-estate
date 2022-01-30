bb9804fb6ebc4b609e580e7e48a8a000852460ce
Donald Biggers <DuG@tests-MacBook-Air.local>
2022-01-30 15:28:42 -0600

    created singleRequest.handlebars and linking routes to login page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,29 +1,94 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Property } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+// const { default: ModelManager } = require("sequelize/dist/lib/model-manager");
+const sequelize = require("../config/connection");
+const { Request } = require("../models");
+const withAuth = require("../utils/auth");
 //Get Routes
 
-router.get('/', withAuth, (req, res) => {
-        Property.findAll({
-            attributes: [
-              "id",
-              "bedrooms",
-              "bathrooms",
-              "rent",
-              "sq_ft",
-              "address_line1",
-              "address_line2",
-              "city",
-              "state",
-              "zip_code",
-              "county",
-            ],
-            include: [{ all: true, nested: false }],
-          })
-            .then((dbUserData) => res.json(dbUserData))
-            .catch((err) => {
-              console.log(err);
-              res.status(500).json(err);
-            });
+router.get("/", (req, res) => {
+  Request.findAll({
+    attributes: ["id", "request_message"],
+    include: [{ all: true, nested: false }],
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Request.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "request_message", "created_at", "updated_at"],
+    include: [{ all: true, nested: false }],
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  Request.create({
+    property_id: req.body.property_id,
+    request_type_id: req.body.request_type_id,
+    request_message: req.body.request_message,
+    status_type_id: req.body.status_type_id,
+    work_order_type_id: req.body.work_order_type_id,
+  })
+    .then((dbRequestData) => res.json(dbRequestData))
+    .catch((err) => {
+      console.log(err);
+      res.json(500).json(err);
+    });
+});
+
+//Put Routes
+
+router.put("/:id", (req, res) => {
+  Request.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+
+    .then((dbRequestData) => {
+      if (!dbRequestData[0]) {
+        res.status(404).json({
+          message: "no request found with that id",
         });
+        return;
+      }
+      res.json(dbRequestData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Delete Routes
+
+router.delete("/:id", (req, res) => {
+  Request.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbRequestData) => {
+      if (!dbRequestData) {
+        res.status(404).json({ message: "no request found with this id" });
+        return;
+      }
+      res.json(dbRequestData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,32 +2,67 @@
 
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const { Property } = require("../models");
+const { Property, Request } = require("../models");
 
-router.get('/', (req, res) => {
-    Property.findAll({
-            attributes: [
-              "bedrooms",
-              "bathrooms",
-              "rent",
-              "sq_ft",
-              "address_line1",
-              "address_line2",
-              "city",
-              "state",
-              "zip_code",
-              "county",
-            ],
-          })
-            .then((dbPropertyData) => {
-              const posts = dbPropertyData.map((post) => post.get({ plain: true }));
-             res.render("homepage", { posts, loggedIn: req.session.loggedIn });
-            })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+router.get("/", (req, res) => {
+  Property.findAll({
+    attributes: [
+      "bedrooms",
+      "bathrooms",
+      "rent",
+      "sq_ft",
+      "address_line1",
+      "address_line2",
+      "city",
+      "state",
+      "zip_code",
+      "county",
+    ],
+  })
+    .then((dbPropertyData) => {
+      const posts = dbPropertyData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    console.log("REDIRECTING");
+    res.redirect("/");
+    return; //removing return causes an error
+  }
+  console.log("Rendinging the login page");
+  res.render("login");
+});
+
+router.get("/request/:id", (req, res) => {
+  Request.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "request_message", "created_at", "updated_at"],
+    include: [{ all: true, nested: false }],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-request", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
